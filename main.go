4f2dcecdbc5cf950dfacbc4f696398fffb2fcc87
urakawa-jinsei <urakawa_jinsei@retail-ai.jp>
2023-11-08 10:20:20 +0900

    [update]router

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	"github.com/urakawa-jinsei/myapi/controllers"
+	"github.com/urakawa-jinsei/myapi/routers"
 	"github.com/urakawa-jinsei/myapi/services"
 )
 
@@ -32,15 +32,7 @@ func main() {
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
 
-	r := mux.NewRouter()
-
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
-
+	r := routers.NewRouter(con)
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
